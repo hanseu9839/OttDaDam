76619fae0f6279f897d5c76570a31cf44b52d947
murramge <dmdgi37@naver.com>
2022-10-18 01:20:05 +0900

    header ui TBU

--- ottdadam_fe/src/OddHeader/OddHeader.jsx
+++ ottdadam_fe/src/OddHeader/OddHeader.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import styles from "./OddHeader.module.css";
 function OddHeader(props) {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <div className={styles.header}>
+      <div className={styles.home}>
+        <i className="fas fa-house"></i>
+      </div>
+      <div className={styles.logo}>
+        <img src="image/logo.png" alt="#" width={50} height={50} />
+      </div>
+    </div>
+  );
 }
 
 export default OddHeader;
