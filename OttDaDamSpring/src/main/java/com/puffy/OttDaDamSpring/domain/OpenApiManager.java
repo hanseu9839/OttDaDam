4170283fe6be69cdf783036f3ffba22a7c01ce65
hanseu9839 <hanseu9839@gmail.com>
2022-10-21 11:23:38 +0900

    OttDaDam Open API data Connect to Spring

--- OttDaDamSpring/src/main/java/com/puffy/OttDaDamSpring/domain/OpenApiManager.java
+++ OttDaDamSpring/src/main/java/com/puffy/OttDaDamSpring/domain/OpenApiManager.java
@@ -9,32 +9,35 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.UnsupportedEncodingException;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLDecoder;
 import java.util.Map;
 
 @Component
 public class OpenApiManager {
     private final String BASE_URL = "https://api.odcloud.kr";
-    private final String apiUri = "/api/15068871/v1/uddi:672059d4-1830-44af-97dd-cf0954b2ee86";
+    private final String apiUri = "/api/15076398/v1/uddi:6dec2a8d-6404-4318-8767-85419b3c45a0?";
     private final String page = "page=1";
     private final String perPage = "&perPage=10";
-    private final String serviceKey = "&serviceKey=Z+QodRDulSnhc/QPPMSjFBJmRBoB2lmzYIoADFND8d/v7M+rFCLklDNG2HS7BZjwCGdMARLWssYcIL5pzX/gSw==";
+    private final String serviceKey = "&serviceKey=Z%2BQodRDulSnhc%2FQPPMSjFBJmRBoB2lmzYIoADFND8d%2Fv7M%2BrFCLklDNG2HS7BZjwCGdMARLWssYcIL5pzX%2FgSw%3D%3D";
 
 
 
     private String makeUrl() throws UnsupportedEncodingException {
         return BASE_URL +
                 apiUri +
-                serviceKey +
                 page +
                 perPage +
                 serviceKey ;
     }
 
-    public ResponseEntity<?> fetch() throws UnsupportedEncodingException {
+    public ResponseEntity<?> fetch() throws UnsupportedEncodingException, URISyntaxException {
         System.out.println(makeUrl());
+        URI uri = new URI(makeUrl());
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<?> entity = new HttpEntity<>(new HttpHeaders());
-        ResponseEntity<Map> resultMap = restTemplate.exchange(makeUrl(), HttpMethod.GET, entity, Map.class);
+        ResponseEntity<Map> resultMap = restTemplate.exchange(uri, HttpMethod.GET, entity, Map.class);
         System.out.println(resultMap.getBody());
         return resultMap;
 
