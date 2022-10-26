2226154abb1fbf654335ddda4b1fd7a53ac5fae2
murramge <dmdgi37@naver.com>
2022-10-26 21:36:47 +0900

    search refactoring

--- ottdadam_fe/src/component/OddMain/OddSearch/OddSearch.jsx
+++ ottdadam_fe/src/component/OddMain/OddSearch/OddSearch.jsx
@@ -4,61 +4,34 @@ import { Button, Form } from "react-bootstrap";
 import { useSearchParams } from "react-router-dom";
 
 function OddSearch(props) {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [guParams, setGuParams] = useState();
-  const [dongParams, setDongParams] = useState();
-  const Citys = {
-    city: "서울시",
-    contry: ["구로구", "양천구"],
-    neighborhood: {
-      guro: ["개봉동", "오류동"],
-      yangcheon: ["목동", "신정동"],
-    },
+  const contry = ["구로구", "양천구"];
+  const neighborhood = {
+    guro: ["개봉동", "오류동"],
+    yangcheon: ["목동", "신정동"],
+    defalutvalue: ["동"],
   };
-  const countyvalue = Citys.contry.map((value) => <option>{value}</option>);
+  const [neighborhoodState, setneighborhoodState] = useState();
+  const [contryState, setContryState] = useState();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const countyvalue = contry.map((value) => <option>{value}</option>);
+  const handleContry = (e) => {
+    const contryvalue = e.target.value;
+    setContryState(contryvalue);
+  };
+  const handleNeighborHood = (e) => {
+    const neighborhoodvalue = e.target.value;
+    setneighborhoodState(neighborhoodvalue);
+  };
+
   const addQuery = (e) => {
     let currentQuery = e.target.dataset.query;
     currentQuery = currentQuery.split(",");
+
     setSearchParams({
       gu: currentQuery[0],
       dong: currentQuery[1],
     });
-  };
-
-  const handleChangeDong = (e) => {
-    const contry = e.target.value;
-    const subcity = document.getElementById("dong");
-    console.log(`neighborhood ${Citys.neighborhood}`);
-    console.log(contry);
-    switch (contry) {
-      case "구로구":
-        Citys.neighborhood = Citys.neighborhood.guro;
-        setGuParams("gurogu");
-        break;
-      case "양천구":
-        Citys.neighborhood = Citys.neighborhood.yangcheon;
-        setGuParams("yangcheongu");
-        break;
-    }
-    subcity.options.length = 0;
-    for (let i = 0; i < Citys.neighborhood.length; i++) {
-      const option = document.createElement("option");
-      option.innerText = Citys.neighborhood[i];
-      subcity.append(option);
-    }
-  };
-
-  const handleDongQuery = (e) => {
-    const mainoption = e.target.value;
-    console.log(mainoption);
-    switch (mainoption) {
-      case "오류동":
-        setDongParams("orudong");
-        break;
-      case "개봉동":
-        setDongParams("gaebongdong");
-        break;
-    }
   };
 
   return (
@@ -72,31 +45,40 @@ function OddSearch(props) {
           <div className={styles.search}>
             <Form.Select size="sm" className={styles.select}>
               <option selected disabled>
-                {Citys.city}
+                서울시
               </option>
             </Form.Select>
             <Form.Select
               size="sm"
               className={styles.select}
-              onChange={handleChangeDong}
+              onChange={handleContry}
             >
+              <option selected disabled>
+                구
+              </option>
               {countyvalue}
             </Form.Select>
             <Form.Select
               size="sm"
               className={styles.select}
-              id="dong"
-              onChange={handleDongQuery}
+              onChange={handleNeighborHood}
             >
-              <option selected disabled>
-                동
-              </option>
+              {contryState === "구로구" ? (
+                neighborhood.guro.map((guro) => <option>{guro}</option>)
+              ) : contryState === "양천구" ? (
+                neighborhood.yangcheon.map((yangcheon) => (
+                  <option>{yangcheon}</option>
+                ))
+              ) : (
+                <option selected disabled>
+                  {neighborhood.defalutvalue}
+                </option>
+              )}
             </Form.Select>
-
             <Button
               className={styles.button}
               variant="outline-success"
-              data-query={[guParams, dongParams]}
+              data-query={[contryState, neighborhoodState]}
               onClick={addQuery}
             >
               검색
